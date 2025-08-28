ea940892728916ddc7701e8c54dd064c31e9573a
ayaSaleh717 <105351734+ayaSaleh717@users.noreply.github.com>
2025-08-28 16:11:48 +0300

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 // Base URL for API and WebSocket connections
-const BASE_URL = "http://localhost:3500/";
+const BASE_URL = "https://chat-app-backnd.onrender.com";
 
 /**
  * Authentication and WebSocket store using Zustand
